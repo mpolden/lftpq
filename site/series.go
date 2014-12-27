96ab91ff6008c55c90c7ecb614dc6ffd99a4103c
Martin Polden <martin.polden@gmail.com>
2014-12-28 00:02:41 +0100

    Implement initial parsing of series

---
+++ site/series.go
@@ -0,0 +1,43 @@
+package site
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var seriesExp = regexp.MustCompile("(.*)\\.(?:(S(\\d{2})E(\\d{2})|(\\d{1,2})x(\\d{2})))")
+
+type Series struct {
+	Name    string
+	Season  string
+	Episode string
+}
+
+func ParseSeries(s string) (Series, error) {
+	m := seriesExp.FindAllStringSubmatch(s, -1)
+	if len(m) == 0 {
+		return Series{}, fmt.Errorf("no matches found")
+	}
+	if len(m[0]) < 7 {
+		return Series{}, fmt.Errorf("only %d submatches found",
+			len(m[0]))
+	}
+	name := strings.Replace(m[0][1], "_", ".", -1)
+	var season string
+	var episode string
+	if m[0][3] != "" && m[0][4] != "" {
+		season = m[0][3]
+		episode = m[0][4]
+	} else if m[0][5] != "" && m[0][6] != "" {
+		season = m[0][5]
+		episode = m[0][6]
+	} else {
+		return Series{}, fmt.Errorf("failed to parse season and episode")
+	}
+	return Series{
+		Name:    name,
+		Season:  season,
+		Episode: episode,
+	}, nil
+}
---
+++ site/series_test.go
@@ -0,0 +1,61 @@
+package site
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseSeries(t *testing.T) {
+	s := "Gotham.S01E01.720p.HDTV.X264-DIMENSION"
+	series, err := ParseSeries(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := Series{
+		Name:    "Gotham",
+		Season:  "01",
+		Episode: "01",
+	}
+	if !reflect.DeepEqual(series, expected) {
+		t.Fatalf("Expected %+v, got %+v", expected, series)
+	}
+}
+
+func TestParseSeries2(t *testing.T) {
+	s := "Top_Gear.21x02.720p_HDTV_x264-FoV"
+	series, err := ParseSeries(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := Series{
+		Name:    "Top.Gear",
+		Season:  "21",
+		Episode: "02",
+	}
+	if !reflect.DeepEqual(series, expected) {
+		t.Fatalf("Expected %+v, got %+v", expected, series)
+	}
+}
+
+func TestParseSeries3(t *testing.T) {
+	s := "Eastbound.and.Down.S02E05.720p.BluRay.X264-REWARD"
+	series, err := ParseSeries(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := Series{
+		Name:    "Eastbound.and.Down",
+		Season:  "02",
+		Episode: "05",
+	}
+	if !reflect.DeepEqual(series, expected) {
+		t.Fatalf("Expected %+v, got %+v", expected, series)
+	}
+}
+
+func TestParseSeries4(t *testing.T) {
+	_, err := ParseSeries("foo")
+	if err == nil {
+		t.Fatal("Expected error")
+	}
+}
