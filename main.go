736f37c759efd76676ebce6085758316657874ff
Martin Polden <martin.polden@gmail.com>
2014-12-29 01:47:10 +0100

    Add test option

--- main.go
+++ main.go
@@ -9,45 +9,57 @@ import (
 	"os"
 )
 
-func main() {
-	var opts struct {
-		Dryrun bool   `short:"n" long:"dryrun" description:"Print generated command instead of running it"`
-		Config string `short:"f" long:"config" description:"Path to config" value-name:"FILE" default:"~/.lftpfetchrc"`
+type CLI struct {
+	Config string `short:"f" long:"config" description:"Path to config" value-name:"FILE" default:"~/.lftpfetchrc"`
+	Dryrun bool   `short:"n" long:"dryrun" description:"Print generated command instead of running it"`
+	Test   bool   `short:"t" long:"test" description:"Test config and exit"`
+}
+
+func (c *CLI) Run(s site.Site) error {
+	dirs, err := s.GetDirs()
+	if err != nil {
+		return err
 	}
-	_, err := flags.ParseArgs(&opts, os.Args)
+	filtered := s.FilterDirs(dirs)
+	cmds := make([]cmd.Lftp, 0, len(dirs))
+	for _, d := range filtered {
+		cmd, err := s.QueueCmd(d)
+		if err != nil {
+			log.Printf("Skipping cmd for %s: %s", d.Path,
+				err)
+			continue
+		}
+		cmds = append(cmds, cmd)
+	}
+	queueCmd, err := cmd.Join(cmds)
+	if err != nil {
+		return err
+	}
+	if c.Dryrun {
+		fmt.Println(queueCmd.String())
+	} else if err := queueCmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	var cli CLI
+	_, err := flags.ParseArgs(&cli, os.Args)
 	if err != nil {
 		os.Exit(1)
 	}
-	cfg, err := site.ReadConfig(opts.Config)
+	cfg, err := site.ReadConfig(cli.Config)
 	if err != nil {
 		log.Fatal(err)
 	}
+	if cli.Test {
+		log.Print("Read config successfully")
+		return
+	}
 	for _, s := range cfg.Sites {
-		dirs, err := s.GetDirs()
-		if err != nil {
+		if err := cli.Run(s); err != nil {
 			log.Fatal(err)
-		}
-		filtered := s.FilterDirs(dirs)
-		cmds := make([]cmd.Lftp, 0, len(dirs))
-		for _, d := range filtered {
-			cmd, err := s.QueueCmd(d)
-			if err != nil {
-				log.Printf("Skipping cmd for %s: %s", d.Path,
-					err)
-				continue
-			}
-			cmds = append(cmds, cmd)
-		}
-		queueCmd, err := cmd.Join(cmds)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if opts.Dryrun {
-			fmt.Println(queueCmd.String())
-		} else {
-			if err := queueCmd.Run(); err != nil {
-				log.Fatal(err)
-			}
 		}
 	}
 }
