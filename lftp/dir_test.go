cb7628f6cdbfd5224d640bec248b7611e2a0a83b
Martin Polden <martin.polden@gmail.com>
2015-09-13 12:48:21 +0200

    Rewrite tests

--- lftp/dir_test.go
+++ lftp/dir_test.go
@@ -7,46 +7,61 @@ import (
 )
 
 func TestParseDir(t *testing.T) {
-	s := "2014-12-16 00:04:30 +0100 CET foo/"
-	dir, err := ParseDir(s)
-	if err != nil {
-		t.Fatal(err)
+	t1 := time.Date(2014, 12, 16, 0, 4, 30, 0, time.FixedZone("CET", 3600))
+	t2 := time.Date(2015, 2, 3, 15, 12, 30, 0, time.FixedZone("CET", 3600))
+	var tests = []struct {
+		in  string
+		out Dir
+	}{
+		{"2014-12-16 00:04:30 +0100 CET /bar/foo/", Dir{Created: t1, Path: "/bar/foo"}},
+		{"2015-02-03 15:12:30 +0100 CET /foo/bar@", Dir{Created: t2, Path: "/foo/bar", IsSymlink: true}},
+		{"2014-12-16 00:04:30 +0100 CET /foo/bar baz/", Dir{Created: t1, Path: "/foo/bar baz"}},
 	}
-	if name := "foo"; dir.Base() != name {
-		t.Fatalf("Expected %s, got %s", name, dir.Base())
+	for _, tt := range tests {
+		d, err := ParseDir(tt.in)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if d.Path != tt.out.Path {
+			t.Errorf("Expected %q, got %q", tt.out.Path, d.Path)
+		}
+		if !d.Created.Equal(tt.out.Created) {
+			t.Errorf("Expected %s, got %s", tt.out.Created, d.Created)
+
+		}
+		if d.IsSymlink != tt.out.IsSymlink {
+			t.Errorf("Expected %t, got %t", tt.out.IsSymlink, d.IsSymlink)
+		}
 	}
-	d := time.Date(2014, 12, 16, 0, 4, 30, 0, time.FixedZone("CET", 3600))
-	if !d.Equal(dir.Created) {
-		t.Fatalf("Expected %s, got %s", d, dir.Created)
+}
+
+func TestBase(t *testing.T) {
+	in := Dir{Path: "/foo/bar"}
+	out := "bar"
+	if got := in.Base(); got != out {
+		t.Fatalf("Expected %q, got %q", out, got)
 	}
 }
 
 func TestAge(t *testing.T) {
 	now := time.Now().Round(time.Second)
-	d1 := Dir{
-		Path:    "/tmp/foo",
-		Created: now,
+	var tests = []struct {
+		in  Dir
+		out time.Duration
+	}{
+		{Dir{Created: now}, time.Duration(0)},
+		{Dir{Created: now.Add(-time.Duration(48) * time.Hour)}, time.Duration(48) * time.Hour},
 	}
-	age := d1.Age()
-	if expected := time.Duration(0); age != expected {
-		t.Errorf("Expected %q, got %q", expected, age)
-	}
-
-	d2 := Dir{
-		Path:    "/tmp/bar",
-		Created: now.Add(-time.Duration(48) * time.Hour),
-	}
-	age = d2.Age()
-	if expected := time.Duration(48) * time.Hour; age != expected {
-		t.Errorf("Expected %q, got %q", expected, age)
+	for _, tt := range tests {
+		if got := tt.in.Age(); got != tt.out {
+			t.Errorf("Expected %s, got %s", tt.out, got)
+		}
 	}
 }
 
 func TestMatch(t *testing.T) {
-	d := Dir{
-		Path:    "/tmp/foo",
-		Created: time.Now(),
-	}
+	d := Dir{Path: "/tmp/foo"}
 	if !d.Match(regexp.MustCompile("f")) {
 		t.Fatal("Expected true")
 	}
@@ -56,10 +71,7 @@ func TestMatch(t *testing.T) {
 }
 
 func TestMatchAny(t *testing.T) {
-	d := Dir{
-		Path:    "/tmp/foo",
-		Created: time.Now(),
-	}
+	d := Dir{Path: "/tmp/foo"}
 	patterns := []*regexp.Regexp{
 		regexp.MustCompile("fo"),
 		regexp.MustCompile("ba"),
